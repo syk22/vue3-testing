b5266dd3fcbd3946f0134d28e4242096d7b15e20
SayakaNakajima <sykrec22@gmail.com>
2023-07-17 01:58:56 +0900

    Added to 'TailwindCSS' to package.json

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 </template>
 
 <style>
-@import '@/assets/style/reset2.scss';
+/* @import '@/assets/style/reset2.scss'; */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
