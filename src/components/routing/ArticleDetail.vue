0036ada8ef188bf955773e5bf1ee8864497fa714
SayakaNakajima <sykrec22@gmail.com>
2023-07-17 22:58:50 +0900

    Fixed error.

--- src/components/routing/ArticleDetail.vue
+++ src/components/routing/ArticleDetail.vue
@@ -5,14 +5,14 @@ import { articlesData } from './articlesData';
 type ArticleList = {
   id: string;
   title: string;
-  article?: string;
-  url?: string
+  article: string;
+  url: string
 };
 const props = defineProps({
   id: String,
 });
 const article: ArticleList | undefined = articlesData.find(item => item.id === props.id);
-const solidArticle = article.article.replaceAll(/\r\n/g, '<br /><br />');
+const solidArticle = article ? article.article.replaceAll(/\r\n/g, '<br /><br />') : '';
 </script>
 
 <template>
